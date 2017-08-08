9d2d3ae06606c658b5b3590a6e8665075a636cea
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2017-08-08 08:26:08 -0700

    Create large.c

---
+++ large.c
@@ -0,0 +1,18 @@
+#inlcude<stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+printf("a is big");
+}
+elseif(b>c)
+{
+printf("b is big");
+}
+else
+{
+printf("c is big");
+}
+}
