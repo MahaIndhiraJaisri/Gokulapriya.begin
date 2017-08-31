8a789f2055470f5fd20364971040f9fa844c19c2
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2017-08-31 22:04:29 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void main()
+{
+char a;
+scanf("%d",&a);
+if(!isdigit(a))
+{
+switch(a)
+{
+case "a":
+         printf("vowel");
+         break:
+case "e":
+         printf("vowel");
+         break:
+case "i":
+         printf("vowel");
+         break:
+case "o":
+         printf("vowel");
+         break:
+case "u":
+         printf("vowel");
+         break:
+case "A":
+         printf("vowel");
+         break:
+case "E":
+         printf("vowel");
+         break:
+case "I":
+         printf("vowel");
+         break:
+case "O":
+         printf("vowel");
+         break:
+case "U":
+         printf("vowel");
+         break:
+default:
+         print("consonent);
+}
+}
+}
