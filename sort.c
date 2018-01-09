171b71a51a671d30720c97b247eda6380d5a8225
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-09 16:08:34 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -3,8 +3,14 @@
 void main()
 {
 int n,arr[20],i=0,min,temp;
-printf("enter array limit");
+printf("enter array limit between 1 to 1000");
 scanf("%d",&n);
+  if(!(n>=1)&&(n<=1000))
+  {
+    printf("limit only with in 1 to 1000");
+  }
+  else
+  {
 printf("enter numbers");
 for(i=0;i<n;i++)
 {
@@ -25,6 +31,7 @@ for(i=0;i<n;i++)
 {
 printf("%d",arr[i]);
 }
+  }
 getch();
 }
 
