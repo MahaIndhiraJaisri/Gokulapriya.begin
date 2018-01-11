7d3c6ee4ed17e6f9ac77c0f11ce512869dd0e353
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-11 16:27:53 +0530

    Update max.c

--- max.c
+++ max.c
@@ -3,5 +3,22 @@
 void main()
 {
 int n,arr[20],i=0,min,temp;
-printf("enter array limit between 1 to 1000");
-scanf("%d",&n);
+printf("enter array elements");
+
+  for(i=0;i<10;i++)
+{
+scanf("%d",&arr[i]);
+}
+min=arr[0];
+for(i=0;i<10;i++)
+{
+if(arr[i]<min)
+{
+temp=arr[i];
+arr[i]=min;
+min=temp;
+}
+}
+printf("maximum element is %d",arr[10]);
+}
+
