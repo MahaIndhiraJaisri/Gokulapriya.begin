c6779022d0927da7b387231d0a8ea9dccfd020c6
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-18 16:01:49 +0530

    Create max_min.c

---
+++ max_min.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,n,min,max;
+printf("enter the no of elements");
+scanf("%d",&n);
+printf("enter array elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+}
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+}
+
