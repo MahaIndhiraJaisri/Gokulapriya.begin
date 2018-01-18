d0db53a474e4dded80b12a692cc41ab4119c4fb8
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-18 16:31:19 +0530

    Create text.c

---
+++ text.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number between 1 to 10");
+scanf("%d",&n);
+switch(n)
+{
+case 1:printf("ONE");
+       break;
+case 2:printf("TWO");
+       break;	 
+case 3:printf("THREE");
+       break;
+case 4:printf("FOUR");
+       break;
+case 5:printf("FIVE");
+       break;
+case 6:printf("SIX");
+       break;
+case 7:printf("SEVEN");
+       break;
+case 8:printf("EIGHT");
+       break;
+case 9:printf("NINE");
+       break;
+case 10:printf("TEN");
+       break;	   
+default:printf("number should be within 1 to 10");	
+}
+}
+
+   
