574bcb01bc6ffd2cb15780a539bbdb686eac3f8b
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-19 08:13:34 +0000

    Create near_grt.c

---
+++ near_grt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if((n%10)==0)
+{
+printf("the given number %d is a mulitiple of 10",n);
+}
+else
+{
+do
+{
+n++;
+}while((n%10)!=0);
+printf("%d is a nearset greater multiple of 10",n);
+}
+}
