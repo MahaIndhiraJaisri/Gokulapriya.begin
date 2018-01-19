e619e960f533d264b7a1555a34ce24f09617330f
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-19 08:16:18 +0000

    Create mul_7.c

---
+++ mul_7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if((n%7)==0)
+{
+printf("number is multiple of 7");
+}
+else
+{
+printf("number is not a multiple of 7");
+}
+}
