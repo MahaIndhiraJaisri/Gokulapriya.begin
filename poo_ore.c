e14c0c1aa3536ff79b133314b0cf94586a0eee4f
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-19 07:48:18 +0000

    Create poo_ore.c

---
+++ poo_ore.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,mul;
+printf("enter the numbers");
+scanf("%d%d",&n1,&n2);
+mul=n1*n2;
+if((mul/2)==0)
+{
+printf("EVEN");
+}
+else
+{
+printf("ODD");
+}
+}
+
+
