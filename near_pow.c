c448e849242afe2e28e4888de1a315d6945e500a
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-19 08:44:42 +0000

    Create near_pow.c

---
+++ near_pow.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n/2;i++)
+{
+if(pow(2,i)>=n)
+{
+printf(" nearest power of 2 is %d",pow(2,++i);
+}
+}
+}
