88d2145c97770cf4812dc813b8862378f8ae833f
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-21 11:35:30 +0530

    Rename Nsum.c to summing.c

---
+++ summing.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[30],n,i;
+printf("enter the limit");
+scanf("%d",&n);
+printf("array elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+printf("sum = %d",sum);
+}
+
