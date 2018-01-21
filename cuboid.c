0408cc7a75fee25178d4a504a760f92c49415c99
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-21 11:18:21 +0530

    Create cuboid.c

---
+++ cuboid.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int i,b,h,sa,vol;
+printf("enter l,b,h:");
+scanf("%d%d%d",&l,&b,&h);
+vol=l*b*h;
+sa=(2*l*w)+(2*l*h)+(2*h*w);
+printf("surface area of cuboid = %d",sa);
+printf("volume of cuboid = %d",vol);
+}
