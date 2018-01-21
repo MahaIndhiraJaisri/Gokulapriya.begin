29ec39bf7aa286a902d049b431f7719a00c49739
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-21 12:12:20 +0530

    Update Nsum.c

--- Nsum.c
+++ Nsum.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 void main()
 {
-int i,sum=0,num;
-scanf("%d",&n);
+int i,sum=0,num,a[30];
+scanf("%d",&num);
 for(i=1;i<=num;i++)
 {
-sum=sum+i;
+scanf("%d",&a[i]);
+sum=sum+a[i];
 }
 }
