6b0945a9a66fb15e067196cbc515e318032b06bf
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-21 12:21:28 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main()
 {
-int n,arr[20],i=0,min,temp;
+int n,arr[20],i=0,j=0,min,temp;
 printf("enter array limit between 1 to 1000");
 scanf("%d",&n);
   if(!(n>=1)&&(n<=1000))
@@ -17,13 +17,15 @@ for(i=0;i<n;i++)
 scanf("%d",&arr[i]);
 }
 min=arr[0];
-for(i=0;i<n;i++)
+    for(j=0;j<n-1;j++)
+    {
+for(i=j;i<n;i++)
 {
-if(arr[i]<min)
+if(arr[i]>arr[j])
 {
 temp=arr[i];
-arr[i]=min;
-min=temp;
+arr[i]=arr[j];
+arr[j]=temp;
 }
 }
 printf("sorted array is");
