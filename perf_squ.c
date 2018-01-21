cb1d1697df7ce21ff37b7c780e86ebd845cff787
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-21 10:05:44 +0530

    Create perf_squ.c

---
+++ perf_squ.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,i;
+pritntf("enter the numbers");
+scanf("%d%d",&n,&n2);
+pro=n1*n2;
+for(i=0;i<(pro/2);i++)
+{
+if((pow(i,2)==pro)
+{
+printf("product is perfect square");
+break;
+}
+}
+}
+
