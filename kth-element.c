45c0a0ed324e806a06fba74c961dd6d0d773eb2f
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-21 11:50:53 +0530

    Update kth-element.c

--- kth-element.c
+++ kth-element.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int a[30],n,i;
+int a[30],n,k,i;
 printf("enter the limit");
-scanf("%d",&n);
+scanf("%d%d",&n,&k);
 printf("array elements");
 for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
-printf("k-th element is: %d",a[k]);
+printf("k-th element is: %d",a[--k]);
 }
 
