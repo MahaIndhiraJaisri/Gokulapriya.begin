734c3de95611e49f6c5c0544d3057175ec9567bd
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-01-23 16:34:35 +0530

    Create Nchange.c

---
+++ Nchange.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n[20],i=0,N;
+printf("enter the string");
+scanf("%s",&n);
+printf("enter N value");
+scanf("%d",&N);
+for(i=0;i<N;i++)
+{
+if(a[i]!=i)
+{
+printf("%d",i);
+}
+}
+}
