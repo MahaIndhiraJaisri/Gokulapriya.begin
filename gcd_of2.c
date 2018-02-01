441928a33c6adcd1ac496beeda451f9cc62b659f
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-02-01 07:43:38 +0000

    Create gcd_of2.c

---
+++ gcd_of2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,min;
+scanf("%d%d",&a,&b);
+min=(a<b)?a:b;
+for(min=1; min <= a && min <= b; ++min)
+{
+if((a%min==0)&&(b%min==0))
+{
+printf("gcd of %d and %d is %d",a,b,min);
+  break;
+}
+}
+}
+
