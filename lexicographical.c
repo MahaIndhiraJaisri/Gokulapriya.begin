ae737fbf357401824eb4c37c2dceae1e4f364972
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-02-02 15:15:09 +0530

    Create lexicographical.c

---
+++ lexicographical.c
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<string.h>
+void main()
+{
+string s[10];
+int n,i;
+printf("enter the no of strings");
+scanf("%d",&n);
+printf("enter strings");
+for(i=0;i<n;i++)
+{
+scanf("%s",&s[i]);
+}
